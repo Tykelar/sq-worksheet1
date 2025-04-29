8ce3a79d8c9dedcdbc52f82f0d2e9ef2c969a4bd
José Henriques <josepedronolasco@gmail.com>
2025-04-29 19:34:45 +0100

    second level header, finish ws1

--- cypress/e2e/ws1.cy.js
+++ cypress/e2e/ws1.cy.js
@@ -1,10 +1,15 @@
 describe('home page spec', () => { 
   it('passes', () => { 
-    cy.visit('<YOUR CLOUDFLARE WORKER URL>') 
-  }) 
+    cy.visit('https://sq-worksheet1.josepedronolasco.workers.dev/'); 
+  }); 
  
   it('h1 contains "MEI"', () => { 
-    cy.visit('<YOUR CLOUDFLARE WORKER URL>'); 
-    cy.get('h1').contains('MEI') 
-  }) 
-}) 
+    cy.visit('https://sq-worksheet1.josepedronolasco.workers.dev/'); 
+    cy.get('h1').contains('MEI'); 
+  });
+
+  it('h2 contains "2024/25"', () => {
+    cy.visit('https://sq-worksheet1.josepedronolasco.workers.dev/');
+    cy.get('h2').contains('2024/25');
+  });
+});
