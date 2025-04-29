d3ed80215fb0025f05e10336cd6e1e0489cdc7ac
José Henriques <josepedronolasco@gmail.com>
2025-04-29 19:48:25 +0100

    chore: add h2 to index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => { 
-  return c.html('<h1>Hello, MEI QS!</h1>') 
+  return c.html('<h1>Hello, MEI QS!</h1> <h2>2024/25<h2>') 
 }) 
 
 
